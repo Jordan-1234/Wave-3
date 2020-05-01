e450286ee4e303c90522b8fafd730951df01c113
Jordan So <jordan.so21@ycdsbk12.ca>
2020-05-01 04:04:53 -0400

    Prime Numbers

--- Prime Numbers.py
+++ Prime Numbers.py
@@ -1,22 +1,13 @@
-import math
-
-
-def primeFactors(n):
-
-    while n % 2 == 0:
-        n = n / 2
-        print(2)
-
-    for i in range(3, int(math.sqrt(n))+1, 2):
-
-        while n % i == 0:
-            print(i, "")
-            n = n / i
-
-    if n >= 2:
-        print(int(n))
+def primefactors(n):
+    factor = 2
+    while factor <= n:
+        if n % factor == 0:
+            print(factor)
+            n /= factor
+        else:
+            factor += 1
 
 
 n = int(input("Enter a number (2 or greater): "))
 print("The prime factors of", n, "are: ")
-primeFactors(n)
+primefactors(n)
