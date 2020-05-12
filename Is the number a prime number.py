28bd8882299e2a7e0af5659ceb8f956a2a6aa7b3
Jordan So <jordan.so21@ycdsbk12.ca>
2020-05-12 01:47:37 -0400

    Shipping Calculator

--- Is the number a prime number.py
+++ Is the number a prime number.py
@@ -3,13 +3,13 @@ def prime(number):
     if number>1:
         for i in range(2,number):
             if (number % i) == 0:
-                print("It is a prime number")
+                print(number , "is not a prime number")
                 break
-            else:
-                print("It is not a prime number") 
-        else: 
-            print("It is a prime number")
+        else:
+            print(number , "is a prime number") 
+    else: 
+        print(number , "is a prime number")
 
-prime(int(input("Input the number ")))            
+prime(int(input("Input the number: ")))            
         
  
--- Shipping Calculator.py
+++ Shipping Calculator.py
@@ -1,3 +1,5 @@
-numOfItems = int(input("Enter the amount of items: "))
-
-print(10.95 + (2.95 * numOfItems))
+numOfItems = int(input("Enter the amount of items purchased: "))
+if numOfItems < 0:
+    print("Incorrect amount of items")
+else: 
+    print("The shipping cost will be" , 10.95 + (2.95 * numOfItems))
