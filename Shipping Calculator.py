424796ef0fad58869d20ad5160e8cc9937c034ff
Jordan So <jordan.so21@ycdsbk12.ca>
2020-05-12 01:59:50 -0400

    Shipping Calculator

--- Shipping Calculator.py
+++ Shipping Calculator.py
@@ -1,5 +1,5 @@
 numOfItems = int(input("Enter the amount of items purchased: "))
-if numOfItems < 0:
+if numOfItems <= 0:
     print("Incorrect amount of items")
 else: 
-    print("The shipping cost will be" , 10.95 + (2.95 * numOfItems))
+    print("The shipping cost will be" , 10.95 + (2.95 * (numOfItems-1)))
